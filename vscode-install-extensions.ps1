cbd502fa285fca252e7d1f0a74af5dc07ee3562b
matt-walters <mgwalters81@gmail.com>
2019-07-26 01:09:30 +1000

    Renamed vscode settings file. Added ps script to install vscode extensions.

---
+++ vscode-install-extensions.ps1
@@ -0,0 +1,23 @@
+# code --list-extensions
+# code --install-extension ms-vscode.cpptools
+# code --uninstall-extension ms-vscode.csharp
+
+code --install-extension 42Crunch.vscode-openapi
+code --install-extension alefragnani.Bookmarks
+code --install-extension Arjun.swagger-viewer
+code --install-extension DotJoshJohnson.xml
+code --install-extension eamodio.gitlens
+code --install-extension humao.rest-client
+code --install-extension ms-python.python
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
+code --install-extension ms-vscode-remote.remote-ssh-explorer        
+code --install-extension ms-vscode-remote.remote-wsl
+code --install-extension ms-vscode-remote.vscode-remote-extensionpack
+code --install-extension ms-vscode.csharp
+code --install-extension ms-vscode.powershell
+code --install-extension ms-vscode.Theme-MarkdownKit
+code --install-extension ms-vscode.vs-keybindings
+code --install-extension redhat.vscode-yaml
+code --install-extension streetsidesoftware.code-spell-checker
